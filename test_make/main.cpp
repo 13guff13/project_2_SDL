94385a9a508cdd3af51f9dad3656c6a2c4f1d35c
13guff13 <13guff13@gmail.com>
2020-12-20 23:47:30 +0100

    add: log resources functon

--- test_make/main.cpp
+++ test_make/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <string>
 #include <SDL.h>
+#include "res_path.h"
 
-/*
- * Lesson 0: Test to make sure SDL is setup properly
- */
-int main(int, char**){
-  if (SDL_Init(SDL_INIT_VIDEO) != 0){
-    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+int main(int argc, char **argv){
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+    std::cerr << "SDL_Init error: " << SDL_GetError() << std::endl;
     return 1;
   }
+  std::cout << "Resource path is: " << getResourcePath() << std::endl;
+
   SDL_Quit();
   return 0;
 }
