a3d06e5af712265d2c43eca61adf8e3a78811257
13guff13 <13guff13@gmail.com>
2020-12-23 11:55:24 +0100

    add: png and other image support

--- test_make/main.cpp
+++ test_make/main.cpp
@@ -1,3 +1,4 @@
+#include "./SDL2_image-2.0.5/SDL2_image-2.0.5/SDL_image.h"
 #include <iostream>
 #include <string>
 #include <SDL.h>
@@ -17,27 +18,15 @@ void logSDLError(std::ostream &os, const std::string &msg){
 }
 
 /**
- * Loads a BMP image into a texture on the rendering device
- * @param file The BMP image file to load
+ * Loads an image into a texture on the rendering device
+ * @param file The image file to load
  * @param ren The renderer to load the texture onto
  * @return the loaded texture, or nullptr if something went wrong.
  */
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
-  //Initialize to nullptr to avoid dangling pointer issues
-  SDL_Texture *texture = nullptr;
-  //Load the image
-  SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
-  //If the loading went ok, convert to texture and return the texture
-  if (loadedImage != nullptr){
-    texture = SDL_CreateTextureFromSurface(ren, loadedImage);
-    SDL_FreeSurface(loadedImage);
-    //Make sure converting went ok too
-    if (texture == nullptr){
-      logSDLError(std::cout, "CreateTextureFromSurface");
-    }
-  }
-  else {
-    logSDLError(std::cout, "LoadBMP");
+  SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
+  if (texture == nullptr){
+    logSDLError(std::cout, "LoadTexture");
   }
   return texture;
 }
@@ -117,14 +106,6 @@ int main(int argc, char **argv){
 
     SDL_RenderPresent(renderer);
     SDL_Delay(1000);
-    // //First clear the renderer
-    // SDL_RenderClear(renderer);
-    // //Draw the texture      
-    // SDL_RenderCopy(renderer, tex, NULL, NULL);
-    // //Update the screen
-    // SDL_RenderPresent(renderer);
-    // //Take a quick break after all that hard work
-    // SDL_Delay(1000);
   }
 
   
