fc75d21ef59b247c4cf6fe21edf84da944d82301
13guff13 <13guff13@gmail.com>
2021-01-07 23:49:06 +0100

    add: text rendering

--- test_make/collision.cpp
+++ test_make/collision.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include "./SDL2_ttf-2.0.15/SDL2_ttf-2.0.15/SDL_ttf.h"
+#include <iostream>
+#include <string>
 #include <SDL.h>
 #include <math.h>
 
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 const int FLOOR = SCREEN_HEIGHT - 60;
-const float GAME_SPEED = 5;
+const float GAME_SPEED = 7;
 const int LEVEL_LENGTH = 1000;
 
 const int IMPEDIMENTS_AMOUNT = 3;
@@ -52,6 +55,94 @@ int collision_with_impediments(SDL_Renderer* renderer, SDL_Rect* character, SDL_
   return 0;
 }
 
+void logSDLError(std::ostream &os, const std::string &msg){
+  os << msg << " error: " << SDL_GetError() << std::endl;
+}
+
+
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h){
+  //Setup the destination rectangle to be at the position we want
+  SDL_Rect dst;
+  dst.x = x;
+  dst.y = y;
+  dst.w = w;
+  dst.h = h;
+  SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+/**
+ * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+ * the texture's width and height
+ * @param tex The source texture we want to draw
+ * @param ren The renderer we want to draw to
+ * @param x The x coordinate to draw to
+ * @param y The y coordinate to draw to
+ */
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
+  float scale = 1;
+  int w, h;
+  SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+  // actual width - w actual height - h
+  // printf("renderTexture: (x=%d,y=%d,w=%d,h=%d, scale=%.2f)", x, y, w, h, scale);
+  // renderTexture(tex, ren, x, y, (int)w*scale, (int)h*scale);
+  SDL_Rect dst;
+  dst.x = x;
+  dst.y = y;
+  dst.w = (int)w*scale;
+  dst.h = (int)h*scale;
+  SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+
+SDL_Texture* renderText(const std::string &message, const std::string &fontFile, SDL_Color color, int fontSize, SDL_Renderer *renderer)
+{
+  //Open the font
+  TTF_Font *font = TTF_OpenFont(fontFile.c_str(), fontSize);
+  if (font == nullptr){
+    logSDLError(std::cout, "TTF_OpenFont");
+      // std::cout << "[JORA1]" << std::endl;
+    return nullptr;
+  }	
+  //We need to first render to a surface as that's what TTF_RenderText
+  //returns, then load that surface into a texture
+  SDL_Surface *surf = TTF_RenderText_Blended(font, message.c_str(), color);
+  if (surf == nullptr){
+    TTF_CloseFont(font);
+    logSDLError(std::cout, "TTF_RenderText");
+    // std::cout << "[JORA2]" << std::endl;    
+    return nullptr;
+  }
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
+  if (texture == nullptr){
+    // std::cout << "[JORA3]" << std::endl;    
+    logSDLError(std::cout, "CreateTexture");
+    // return nullptr;    
+  }
+  //Clean up the surface and font
+  SDL_FreeSurface(surf);
+  TTF_CloseFont(font);
+  return texture;
+}
+
+void view_foreground_text(SDL_Renderer *renderer, SDL_Window *window)
+{
+  SDL_Color color = { 255, 255, 255, 255 };
+  SDL_Texture *text = renderText("Fuck project 2!", "./res/linux-libertine/LinLibertine_aBS.ttf",
+				  color, 64, renderer);
+
+  if (text == nullptr){
+    // cleanup(renderer, window);
+    std::cout << "[JORA]" << std::endl;    
+    TTF_Quit();
+    SDL_Quit();
+    return ;
+  }
+  //Get the texture w/h so we can center it in the screen
+  int iW, iH;
+  SDL_QueryTexture(text, NULL, NULL, &iW, &iH);
+  int x = SCREEN_WIDTH / 2 - iW / 2;
+  int y = SCREEN_HEIGHT / 2 - iH / 2;
+  
+  renderTexture(text, renderer, x, y);  
+}
 
 int main(int argc, char ** argv)
 {
@@ -66,11 +157,19 @@ int main(int argc, char ** argv)
 
 
   // init SDL
-
   SDL_Init(SDL_INIT_VIDEO);
-  SDL_Window * window = SDL_CreateWindow("SDL2 Bounding Box Collision Detection",
+  SDL_Window * window = SDL_CreateWindow("Unicorn su*k",
 					 SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
   SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+
+  //------ init SDL_TTF lib --------
+  if (TTF_Init() != 0){
+    logSDLError(std::cout, "TTF_Init");
+    SDL_Quit();
+    return 1;
+  }
+
+  
 
   SDL_Rect ground = { 0, FLOOR, SCREEN_WIDTH, 70};
 
@@ -151,6 +250,8 @@ int main(int argc, char ** argv)
 
       
       SDL_RenderFillRect(renderer, &rect2);
+
+      view_foreground_text(renderer, window);
 
       SDL_RenderPresent(renderer);
     }
